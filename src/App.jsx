031ba1297d691ab0f212de74ff8aa2d3fbde59e7
Titania01 <adegbitebidmus@gmail.com>
2022-02-21 15:15:56 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import Content from "./Content";
+import Header from "./Header";
+import Total from "./Total";
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = { name: "Fundamentals of React", exercises: 10 };
+  const part2 = { name: "Using props to pass data", exercises: 7 };
+  const part3 = { name: "State of a component", exercises: 14 };
+
+  const total = part1.exercises + part2.exercises + part3.exercises;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        exercises1={part1.exercises}
+        exercises2={part2.exercises}
+        exercises3={part3.exercises}
+        part1={part1.name}
+        part2={part2.name}
+        part3={part3.name}
+      />
+      <Total total={total} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Total.jsx
@@ -0,0 +1,5 @@
+const Total = (props) => {
+  return <div>Total Number of Exercises are {props.total}</div>;
+};
+
+export default Total;
